3bd49d8170ab8d67987eb70bcfa904e31db8c5ce
kitloko <marcusreis13@outlook.com>
2022-06-21 20:41:52 -0300

    Initial Commit

---
+++ api-gateway-service/src/test/java/br/com/roobox/apigatewayservice/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roobox.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ chat-bot/src/main/java/br/com/roobox/chatbot/ChatBotApplication.java
@@ -0,0 +1,15 @@
+package br.com.roobox.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotApplication.class, args);
+	}
+
+}
